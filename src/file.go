390f06b015153f91ce8345f9ad0ef7b7d5ad8568
kingeast <1207877378@qq.com>
2024-03-22 02:01:28 +0800

    upd

--- src/file.go
+++ src/file.go
@@ -5,7 +5,7 @@ import "strings"
 // 获取路径内的文件名
 func getFilename(val string) string {
 	temps := strings.Split(val, "/")
-	if len(temps) > 0 {
+	if len(temps) == 0 {
 		return val
 	}
 	return temps[len(temps)-1]
