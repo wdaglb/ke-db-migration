0ba270a78926c190c549862e3cca5c6076c981a1
wangdong <1207877378@qq.com>
2024-06-03 14:09:33 +0800

    配置文件支持${ENV}读取环境变量

--- src/migration.go
+++ src/migration.go
@@ -59,7 +59,6 @@ func Migration() {
 			core.DB.Create(&data)
 		}
 
-		sqlList := strings.Split(sql, ";")
 		err = core.DB.Transaction(func(tx *gorm.DB) error {
 			sqlList := strings.Split(sql, ";")
 			for _, sqlItem := range sqlList {
