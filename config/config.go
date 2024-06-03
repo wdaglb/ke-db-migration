8d632835372bf958d90c0dff4ca65edbe5579102
wangdong <1207877378@qq.com>
2024-06-03 14:03:05 +0800

    配置文件支持${ENV}读取环境变量

--- config/config.go
+++ config/config.go
@@ -1,10 +1,13 @@
 package config
 
 import (
+	"bufio"
 	"flag"
 	"gopkg.in/yaml.v3"
 	"log"
 	"os"
+	"regexp"
+	"strings"
 )
 
 var Config config
@@ -19,14 +22,52 @@ type config struct {
 }
 
 func init() {
-	dir := flag.String("config", "config.yml", "配置文件")
-
-	dataBytes, err := os.ReadFile(*dir)
+	configPath := flag.String("config", "config.yml", "配置文件")
+	flag.Parse()
+	dataBytes, err := parseConfig(*configPath)
 	if err != nil {
 		log.Fatal(err)
 	}
+
 	err = yaml.Unmarshal(dataBytes, &Config)
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+func parseConfig(configSrc string) ([]byte, error) {
+	// 打开配置文件
+	file, err := os.Open(configSrc)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	// 创建一个正则表达式，用于匹配 ${变量名} 格式的字符串
+	re := regexp.MustCompile(`\${(\w+)}`)
+
+	// 读取文件内容并进行变量替换
+	var result strings.Builder
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		replacedLine := re.ReplaceAllStringFunc(line, func(match string) string {
+			// 提取变量名
+			varName := re.FindStringSubmatch(match)[1]
+			// 获取环境变量值
+			envValue := os.Getenv(varName)
+			if envValue == "" {
+				// 如果环境变量不存在，保留原始字符串
+				return match
+			}
+			return envValue
+		})
+		result.WriteString(replacedLine + "\n")
+	}
+
+	if err := scanner.Err(); err != nil {
+		return nil, err
+	}
+
+	return []byte(result.String()), nil
 }
