eff38971fbcca2816e89c5f9796a4477859aff40
wangdong <1207877378@qq.com>
2024-06-28 18:03:02 +0800

    :feat: 支持过滤sql注释

--- utils/string.go
+++ utils/string.go
@@ -1,9 +1,13 @@
 package utils
 
-import "strings"
+import (
+	"regexp"
+	"strings"
+)
 
 // FilterSpaceLine 过滤空行
 func FilterSpaceLine(s string) string {
+	s = removeComments(s)
 	// 将输入字符串按行分割
 	lines := strings.Split(s, "\n")
 
@@ -19,4 +23,19 @@ func FilterSpaceLine(s string) string {
 
 	// 将过滤后的行连接成一个新的字符串
 	return strings.Join(filteredLines, "\n")
+}
+
+// 从 SQL 语句中移除注释
+func removeComments(sql string) string {
+	// 正则表达式模式，用于匹配 SQL 注释
+	singleLineComment := regexp.MustCompile(`--.*\n`)
+	multiLineComment := regexp.MustCompile(`/\*.*?\*/`)
+
+	// 移除单行注释
+	sql = singleLineComment.ReplaceAllString(sql, "")
+
+	// 移除多行注释
+	sql = multiLineComment.ReplaceAllString(sql, "")
+
+	return sql
 }
